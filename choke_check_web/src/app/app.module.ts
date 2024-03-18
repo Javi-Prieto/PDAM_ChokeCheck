aa4bff4a9d420cff04d8b263777ecbbd2c38cb25
Javier Prieto <jpojaviprior@gmail.com>
2024-03-18 14:54:28 +0100

    finished the navbar of the web part of the project

--- choke_check_web/src/app/app.module.ts
+++ choke_check_web/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './ui/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { GymPageComponent } from './ui/gym-page/gym-page.component';
+import { UsersPageComponent } from './ui/users-page/users-page.component';
+import { TournamentsPageComponent } from './ui/tournaments-page/tournaments-page.component';
+import { SettingsPageComponent } from './ui/settings-page/settings-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    GymPageComponent,
+    UsersPageComponent,
+    TournamentsPageComponent,
+    SettingsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
---
+++ choke_check_web/src/app/ui/gym-page/gym-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gym-page',
+  templateUrl: './gym-page.component.html',
+  styleUrl: './gym-page.component.css'
+})
+export class GymPageComponent {
+
+}
