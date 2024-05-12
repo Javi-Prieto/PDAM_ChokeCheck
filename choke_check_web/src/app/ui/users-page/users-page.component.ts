7eaef0e4a768d6da2afa26dbc72f9508f6863d8e
Javi Prieto <87131712+Javi-Prieto@users.noreply.github.com>
2024-05-12 21:57:28 +0200

    FEAT-06 finished the edition of users

--- choke_check_web/src/app/ui/users-page/users-page.component.ts
+++ choke_check_web/src/app/ui/users-page/users-page.component.ts
@@ -4,6 +4,7 @@ import { User, UserListResponse } from '../../models/response/user-list-response
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateUserRequest } from '../../models/request/create-user-request.interface';
 import { environment } from '../../../environments/environment.development';
+import { EditUserRequest } from '../../models/request/edit-user-request.interface';
 
 @Component({
   selector: 'app-users-page',
@@ -21,6 +22,8 @@ export class UsersPageComponent {
   surNameErr: String = '';
   password: String = '';
   passwordErr: String = '';
+  repeatPassword: String = '';
+  repeatPasswordErr:String = '';
   email: String = '';
   emailErr: String = '';
   height: Number = 0;
@@ -32,6 +35,8 @@ export class UsersPageComponent {
   age: Number = 0;
   ageErr: String = '';
   userRol: Number = 0;
+  userId: String = '';
+  isEditting = false;
 
 
 
@@ -56,6 +61,15 @@ export class UsersPageComponent {
     throw new Error('Method not implemented.');
   }
   open(content: TemplateRef<any>) {
+    this.userName = '';
+    this.name = '';
+    this.surName = '';
+    this.email = '';
+    this.height = 0;
+    this.weight = 0;
+    this.age = 0;
+    this.userId = '';
+    this.isEditting = false;
 		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
 			(result) => {
 				console.log( `Closed with: ${result}`);
@@ -66,7 +80,8 @@ export class UsersPageComponent {
 		);
 	}
   toSave(){
-    let toSave = new CreateUserRequest(this.userName, this.password, this.name, this.surName, this.height, this.weight, this.email, this.age, this.userBelt, this.userRol, this.userSex);
+    if(this.userId == ''){
+      let toSave = new CreateUserRequest(this.userName, this.password, this.name, this.surName, this.height, this.weight, this.email, this.age, this.userBelt, this.userRol, this.userSex);
     this.userService.createUser(toSave).subscribe({
       next: data => {
         window.location.href = `${environment.localHost}users`
@@ -105,6 +120,48 @@ export class UsersPageComponent {
         }
       }
     });
+    }else{
+      let toEdit = new EditUserRequest(this.password, this.name, this.surName, this.height, this.weight, this.email, this.age, this.userBelt, this.userRol, this.userSex);
+      this.userService.editUser(this.userId, toEdit).subscribe({
+        next: data => {
+          window.location.href = `${environment.localHost}users`
+        },error: err => {
+          if(err.status == 400){
+            let badReq = err.error;
+            console.log(badReq);
+            let errors = badReq.body.fields_errors;
+            errors.forEach((error: { field: string; message: string; }) => {
+              if(error.field == "name"){
+                this.nameErr = error.message;
+              }else if(error.field == "password"){
+                this.passwordErr = error.message;
+              }else if(error.field == "username"){
+                this.userNameErr = error.message;
+              }else if(error.field == "surname"){
+                this.surNameErr = error.message;
+              }else if(error.field == "beltColor"){
+                console.log(error.message);
+              }else if(error.field == "height"){
+                this.heightErr = error.message;
+              }else if(error.field == "weight"){
+                this.weightErr = error.message;
+              }else if(error.field == "email"){
+                this.emailErr = error.message;
+              }else if(error.field == "age"){
+                this.ageErr = error.message;
+              }else if(error.field == "rol"){
+                console.log(error.message);
+              }else if(error.field == "sex"){
+                console.log(error.message);
+              }else{
+                alert("NOOOOO MI PAGE");
+              }
+            });
+          }
+        }
+      });
+    }
+    
   }
 
   deleteUser(id:String){
@@ -115,5 +172,25 @@ export class UsersPageComponent {
         console.log(err.error.message);
       }
     });
+  }
+
+  openEditModal(user: User,content: TemplateRef<any>){
+    this.userName = user.username;
+    this.name = user.name;
+    this.surName = user.surname;
+    this.email = user.email;
+    this.height = user.height;
+    this.weight = user.weight;
+    this.age = user.age;
+    this.userId = user.id;
+    this.isEditting = true;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				console.log( `Closed with: ${result}`);
+			},
+			(reason) => {
+				console.log(`${reason}`);
+			},
+		);
   }
 }
